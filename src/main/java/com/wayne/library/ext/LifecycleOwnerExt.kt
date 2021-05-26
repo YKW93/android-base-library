0b364cbc93f1a657466c77921909954bbb2320d1
YKW93 <rhkddn1657@gmail.com>
2021-05-26 22:06:30 +0900

    refactor: internal 삭제

--- src/main/java/com/wayne/library/ext/LifecycleOwnerExt.kt
+++ src/main/java/com/wayne/library/ext/LifecycleOwnerExt.kt
@@ -4,8 +4,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 
-internal fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, action: (T) -> Unit) =
+fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, action: (T) -> Unit) =
     liveData.observe(this) { action(it) }
 
-internal fun <T : Any, L : LiveData<T>> Fragment.observe(liveData: L, action: (T) -> Unit) =
+fun <T : Any, L : LiveData<T>> Fragment.observe(liveData: L, action: (T) -> Unit) =
     liveData.observe(viewLifecycleOwner) { action(it) }
